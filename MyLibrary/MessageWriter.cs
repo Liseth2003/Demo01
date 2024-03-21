c90150a160e9063edba63e3603adb31e47163a60
Dianlly Colorado <msdsc4@gmailcom>
2024-03-21 18:24:23 -0500

    Creacion de clase Console y Library

--- MyLibrary/MessageWriter.cs
+++ MyLibrary/MessageWriter.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyLibrary
+﻿namespace MyLibrary
 {
-    internal class MessageWriter
+    public class MessageWriter
     {
+        public void Writer(string message)
+        {
+            Console.WriteLine(message);
+        }
     }
 }
